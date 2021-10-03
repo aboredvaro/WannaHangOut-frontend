db0dcb04ab6fd40e1dbef19744373110a5e5d293
Carmelo Molero <carmoca6@inf.upv.es>
2021-10-03 16:51:09 +0200

    Create poblarBD.sql
    Empiezo a introducir datos en tablas que sé que no voy a tener que modificar.

---
+++ consultasSQL/poblarBD.sql
@@ -0,0 +1,30 @@
+USE PIN;
+
+
+INSERT INTO rol VALUES
+    (1, "Shop"),
+    (2, "User");
+
+
+INSERT INTO tags VALUES
+    (1, "Aire libre"),
+    (2, "Deportes"),
+    (3, "Gastronomía"),
+    (4, "Hacer Deporte"),
+    (5, "Playa"),
+    (6, "Senderismo"),
+    (7, "Salir de fiesta"),
+    (8, "Pasear"),
+    (9, "Ir de compras");
+
+
+INSERT INTO entity VALUES
+
+
+INSERT INTO taste VALUES
+
+
+INSERT INTO activity VALUES
+
+
+INSERT INTO review VALUES
