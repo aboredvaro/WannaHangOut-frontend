0f8e405af5bbb62e34b1347cfc7e11c3a790ae14
aboredvaro <34165099+aboredvaro@users.noreply.github.com>
2021-10-07 01:31:22 +0200

    Stable Tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,17 @@ import React from 'react'
 const Home = (props) => {
   return (
     <>
-      <div className="w-full h-screen flex items-center justify-center font-medium text-2xl">
-        Tailwind works
+      <div className="w-full h-screen flex flex-col space-y-12 py-24 items-center font-medium">
+        
+        <h1 className="text-4xl">Wanna Hang Out</h1>
+
+        <div className="flex flex-col space-y-2 items-center">
+          <a href="login" className="text-xl text-orange-500">Log in</a>
+          <a href="signup" className="text-xl text-orange-500">Sign up</a>
+          <a href="discover" className="text-xl text-orange-500">Activity search</a>
+          <a href="new" className="text-xl text-orange-500">Create new activity</a>
+        </div>
+
       </div>
     </>
   )
