479b7512aa86d1b0ce6cd35d5e61e8471c41f548
aboredvaro <34165099+aboredvaro@users.noreply.github.com>
2021-10-07 20:06:32 +0200

    Added ESLint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,17 @@
-import App, { AppProps, AppContext } from 'next/app';
+import App, { AppProps, AppContext } from 'next/app'
 import Head from 'next/head'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Wanna HangOut</title>
-        <meta property="og:title" content="Wanna HangOut" key="title" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+	return (
+		<>
+			<Head>
+				<title>Wanna HangOut</title>
+				<meta property="og:title" content="Wanna HangOut" key="title" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
-export default MyApp;
+export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
 
 const Home = (props) => {
-  return (
-    <>
-      <div className="w-full h-screen flex flex-col space-y-12 py-24 items-center font-medium">
+	return (
+		<>
+			<div className="w-full h-screen flex flex-col space-y-12 py-24 items-center font-medium">
         
-        <h1 className="text-4xl">Wanna Hang Out</h1>
+				<h1 className="text-4xl">Wanna Hang Out</h1>
 
-        <div className="flex flex-col space-y-2 items-center">
-          <a href="login" className="text-xl text-orange-500">Log in</a>
-          <a href="signup" className="text-xl text-orange-500">Sign up</a>
-          <a href="discover" className="text-xl text-orange-500">Activity search</a>
-          <a href="new" className="text-xl text-orange-500">Create new activity</a>
-        </div>
+				<div className="flex flex-col space-y-2 items-center">
+					<a href="login" className="text-xl text-orange-500">Log in</a>
+					<a href="signup" className="text-xl text-orange-500">Sign up</a>
+					<a href="discover" className="text-xl text-orange-500">Activity search</a>
+					<a href="new" className="text-xl text-orange-500">Create new activity</a>
+				</div>
 
-      </div>
-    </>
-  )
-};
+			</div>
+		</>
+	)
+}
 
-export default Home;
+export default Home
