cf30b26437ae0437ecb716c209308b0113f2da27
pacoarjonaj <pacoarjonaj@users.noreply.github.com>
2021-10-16 17:24:54 +0200

    all implemented (not sure), error import in activities

--- components/activity-item.tsx
+++ components/activity-item.tsx
@@ -1,14 +1,22 @@
-import React, { useState } from 'react'
+import React  from 'react'
 
 const ActivityItem = (props) => {
 	
-	// Item de actividad que se muestra en la lista de actividades
-
 	return (
 		<>
-			<div>Actividad</div>
+			<div>
+				<h1> 
+					Activity: {props.title} </h1>
+					Description: {props.description}
+					Seats: {props.seats}
+					Price: {props.price}
+					Location: {props.location}
+					Date: {props.dateAct}
+					Duration (min): {props.min_duration}
+			</div>
 		</>
 	)
 }
 
 export default ActivityItem
+
