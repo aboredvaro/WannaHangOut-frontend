e548980eb76bb1c89aef6cee48e4a7fcd9b32884
aboredvaro <34165099+aboredvaro@users.noreply.github.com>
2021-11-08 01:07:16 +0100

    Login, logout and session management is now working

---
+++ utils/session.js
@@ -0,0 +1,31 @@
+import url from './server.js'
+
+export const session = () => {
+	return window.localStorage.getItem('sessionID') ? true : false
+}
+
+export const setSession = async(mail, password) => {
+	return await fetch(`${url}/api/isEntityRegistred`, {
+  	method: 'POST',
+  	headers: {
+    	'Accept': 'application/json',
+    	'Content-Type': 'application/json'
+  	},
+  	body: JSON.stringify({mail: mail, pass: password})
+	}).then(
+		response => response.json()
+	).then(
+		userExists => {
+			if (userExists) {
+				window.localStorage.setItem('sessionID', mail)
+				return true
+			}
+			return false
+		}
+	)
+}
+
+export function exitSession() {
+	window.localStorage.removeItem('sessionID')
+	return true
+}
