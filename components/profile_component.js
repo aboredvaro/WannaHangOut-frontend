cb75bb93c245c8055bbe5f4ff9c6e4ae71485a99
Rossalyna_Jie <53874270+Rossalyna@users.noreply.github.com>
2021-11-16 09:40:28 +0100

    Update

---
+++ components/profile_component.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+const ProfileComponent = (
+    id_entity,
+    id_role,
+    id_address,
+    nick,
+    name,
+    surname,
+    description,
+    mail,
+    phone,
+    avatar
+) =>{
+
+    return (
+        <>
+        
+        </>
+    )
+
+}
+
+export default ProfileComponent
