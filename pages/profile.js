f58b4d65d9dafdade6790f6629d295fbcf3c80f7
Rossalyna_Jie <53874270+Rossalyna@users.noreply.github.com>
2021-11-18 12:33:11 +0100

    Update profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -1,15 +1,43 @@
 import React, { useState } from 'react'
 import Navbar from '../components/navbar'
 
-const Profile = (props) => {
+const Profile = (entity) => {
+
+	function EntityRole() {
+		if(entity.id_role.toString() === '2') {
+			return (
+				<>
+					{/*Teléfono de contacto y email*/}
+				</>
+			)
+		}
+	}
 
 	return (
 		<>
 			{
 				<Navbar/>
 			}
+
+			{/*Componente profile*/}
+			{EntityRole()}
 		</>
 	)
+}
+
+export async function getServerSideProps(ctx) {
+
+	const {id} = ctx.query
+
+	const entity = await fetch(`${url}/api/getEntityByID?id_activity=${id}`)
+	 	.then(response => response.json())
+
+	return {
+		props: {
+			entity
+		}
+	}
+
 }
 
 export default Profile
