c4199b7fa7021956e8c771c9023ac443f5a7e6c4
Karmelo1984 <79457759+Karmelo1984@users.noreply.github.com>
2021-12-11 14:51:55 +0100

    Update cloudinary.js
    Se ha documentado la API

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -11,6 +11,12 @@ import log from './log.js'
 import process from 'process'
 import cloudinary from 'cloudinary'
 
+/**
+ * @description     Dada la ruta en local de imagen en jpg, la aloja en nuestra cuenta de Cloudinary en formato webp
+ * @param {*} image Ruta de la imagen a subir
+ * @param {*} type  Tipo de la imagen 'avatar', 'review' o 'activity'
+ * @returns         Devuelve un string con la url publica de la imagen que se ha almacenado.
+ */
 export async function putImageIntoCloudinary(image, type) {
      cloudinary.v2.config({
           cloud_name: `${process.env.CLOUDINARY_CLOUDNAME}`, 
@@ -31,6 +37,12 @@ export async function putImageIntoCloudinary(image, type) {
      
 }
 
+/**
+ * @description          Dado un array de rutas en local de imágenes en jpg, las aloja en nuestra cuenta de Cloudinary en formato webp
+ * @param {*} images     Array de Rutas de imágenes para subir
+ * @param {*} type       Tipo de la imagen 'avatar', 'review' o 'activity', todas las imágenes deben tener el mismo tipo
+ * @returns              Devuelve un array de string con las urls publicas de cada imagen que se ha almacenado.
+ */
 export async function putImagesIntoCloudinary(images, type) {
      let urls =[]
 
@@ -41,6 +53,11 @@ export async function putImagesIntoCloudinary(images, type) {
      return urls
 }
 
+/**
+ * @description     Dependiendo del tipo de imagen, devuelve la ruta donde se guardará en nuestro servidor
+ * @param {*} type  Tipo de la imagen 'avatar', 'review' o 'activity'
+ * @returns         Ruta donde se debe guardar la imagen
+ */
 function pathOfImage(type) {
      let pathSave 
      if ( type == 'avatar' ) {
