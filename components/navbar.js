a042d51c139e500439a2e4c8fcb35f72042a2d18
Álvaro Hernández <34165099+aboredvaro@users.noreply.github.com>
2021-12-15 02:57:41 +0100

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -191,7 +191,7 @@ const Navbar = ({}) => {
 								<div>
 									<Menu.Button className='relative flex flex-row items-center h-10 px-1.5 space-x-2'>
 										<div className={`relative flex flex-row w-8 h-8 bg-gray-100 ${(sessionID === false) ? 'animate-pulse' : ''} ring-1 ring-gray-200 rounded-full overflow-hidden`}>
-											{sessionID && <img src={sessionID.avatar} className='absolute w-full h-full rounded-full' alt='user avatar' />}
+											{sessionID && <img src={sessionID.avatar} className='absolute w-full h-full rounded-full object-cover' alt='user avatar' />}
 										</div>
 
 										<div className='hidden md:flex flex-row items-center'>
