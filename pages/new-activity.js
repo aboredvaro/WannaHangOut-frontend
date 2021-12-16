d10ea1631a3fee887006eafc05e28e684b54f347
Rossalyna_Jie <53874270+Rossalyna@users.noreply.github.com>
2021-12-16 05:46:11 +0100

    a

--- pages/new-activity.js
+++ pages/new-activity.js
@@ -3,37 +3,13 @@ import url from '../utils/server'
 import log from '../utils/log'
 import { useRouter } from 'next/router'
 import { session, getSession } from '../utils/session'
+import Navbar from '../components/navbar'
 
 const CreateActivity = ({
 	tags
 }) => {
 
 	const router = useRouter()
-
-	const title = useRef()
-	const description = useRef()
-	const seats = useRef()
-	const price = useRef()
-	const date = useRef()
-	const duration = useRef()
-	const postalcode = useRef()
-	const actlocation = useRef()
-	const direction = useRef()
-
-	const [sessionID, setSessionID] = useState()
-
-	useEffect(() => {
-		const getUserSession = async() => {
-			!session() && router.push('/') // Redirect user if not logged in
-
-			const userHash = getSession()
-			
-			setSessionID(await fetch(`${url}/api/getEntityByHash?entityHash=${userHash}`)
-				.then(response => response.json())
-				.then(response => response.id_entity))
-		}
-		getUserSession()
-	})
 
 	function getSelected(){
 		var array = []
@@ -43,104 +19,306 @@ const CreateActivity = ({
 			array.push(checkboxes[i].value)
 		}
 
-		return array
+		return array.length>0?array:[]
 	}
 
-	const handleSubmit = async () => {
+	const [titleValue, setTitle] = useState('')
+	const [descriptionValue, setDescription] = useState('')
+	const [seatsValue, setSeats] = useState('')
+	const [priceValue, setPrice] = useState('')
+	const [durationValue, setDuration] = useState('')
+	const [dateValue, setDate] = useState(Date.now())
+	const [directionValue, setDirection] = useState('')
+	const [codPosValue, setCodPos] = useState('')
+	const [locationValue, setLocation] = useState('')
+	const [isLogged, setIsLogged] = useState(null)
+	const [sessionID, setSessionID] = useState(false)
+	const [urlPath, setUrlPath] =useState('')
 
-		await fetch(
+	/*
+	const date = new Date(activity.dateAct)
+	const yy = date.getFullYear()
+	const mm = date.getMonth()
+	const dd = date.getDay()
+	*/
+
+	const handleCancel = async event => {
+		router.push('/')
+	}
+
+	function handleShowTags() {
+		setShowTags(true)
+	}
+
+	function formatDate(date) {
+		var d = new Date(date),
+			month = '' + (d.getMonth() + 1),
+			day = '' + d.getDate(),
+			year = d.getFullYear();
+	
+		if (month.length < 2) 
+			month = '0' + month;
+		if (day.length < 2) 
+			day = '0' + day;
+	
+		return [year, month, day].join('-');
+	}
+
+	const handleSubmit = async event => {
+		event.preventDefault()	
+		
+		var auxTags = getSelected()
+
+		const res = await fetch(
 			`${url}/api/createNewActivity`,{
-				body: JSON.stringify({
+				body: JSON.stringify({	
 					id_entity_creator: sessionID,
-					title: title.current.value,
-					description: description.current.value,
-					seats: seats.current.value,
-					price: price.current.value,
-					dateAct: date.current.value,
-					min_duration: duration.current.value,
-					tags_act: getSelected(),
-					codPos: postalcode.current.value,
-					location: actlocation.current.value,
-					direction: direction.current.value,
+					title: titleValue,
+					description: descriptionValue,
+					seats: seatsValue,
+					price: priceValue,
+					dateAct: dateValue,
+					min_duration: durationValue,
+					tags_act: auxTags,
+					deleted: 0,
+					codPos: codPosValue,
+					location: locationValue,
+					direction: directionValue,
 				}),
 				headers: {
 					'Content-Type': 'application/json'
 				},
 				method: 'POST'
-			})
-			.then(response => {
-				log('HOLAAAAAAA')
-				log(response)
-			})
-			.then(response => {
-				// router.push(`/activity?id=${response}`)
-				router.push(`/profile?id=${sessionID}`)
-			})
+			}
+		)
+		if(!isNaN(res)) {
+			router.push('activity?id=' + res)
+		}
 
-	}
+	}	
+
+	useEffect(() => {
+        const getUserSession = async() => {
+            const userSession = session()
+            setIsLogged(userSession)
+
+            if (userSession) {
+                const userHash = getSession()
+                const userID = await fetch(`${url}/api/getEntityByHash?entityHash=${userHash}`)
+                    .then(response => response.json())
+                
+                setSessionID(userID)
+            }
+        }
+        getUserSession()
+    }, [])
 
 	return (
-		<div className={`${sessionID ? 'flex' : 'hidden'} flex-col w-full h-screen space-y-12 my-24 items-center`}>
+		<>
+			<Navbar />
+			
+			<div className='flex flex-col w-full h-full items-center justify-center py-20'>
 
-			<h1 className="text-4xl font-medium">Crear Nueva Actividad</h1>
+				{/*Caja blanca */}
+				<form
+					onSubmit={handleSubmit}
+					className="flex flex-col w-110 p-8 rounded-xl shadow-card bg-white border border-gray-100 space-y-4"
+				>
+					<div className="flex flex-col justify-between space-y-10">
+						<div className='flex flex-row w-full justify-center'>
+							<p className="text-2xl text-gray-600 font-medium">Cree su evento</p>
+						</div>
 
-			<form className="flex flex-col space-y-4" id="form">
-				<div>
-					<label className="text-gray-800"htmlFor="title">Titulo </label>
-					<input className="rounded-lg border border-gray-600 focus:border-gray-600" type="text" id="title" name="title" placeholder="Titulo" ref={title}/>
-				</div>
-				<div>
-					<label htmlFor="description">Descripcion </label>
-					<input className="rounded-lg border border-gray-600 focus:border-gray-600" type="text" id="description" name="description" placeholder="Descripcion" ref={description}/>
-				</div>
-				<div>
-					<label htmlFor="seats">Aforo </label>
-					<input className="rounded-lg border border-gray-600 focus:border-gray-600" type="text" id="seats" name="seats" placeholder="Aforo" ref={seats}/>
-				</div>
-				<div>
-					<label htmlFor="price">Precio </label>
-					<input className="rounded-lg border border-gray-600 focus:border-gray-600" type="text" id="price" name="price" placeholder="Precio"  ref={price}/>
-				</div>
-				<div>
-					<label htmlFor="date">Fecha </label>
-					<input className="rounded-lg border border-gray-600 focus:border-gray-600" type="date" id="date" name="date" ref={date}/>
-				</div>
-				<div>
-					<label htmlFor="duration">Duracion </label>
-					<input className="rounded-lg border border-gray-600 focus:border-gray-600" type="text" id="duration" name="duration" placeholder="Duracion" ref={duration}/>
-				</div>
-				<div>
-					<label >Elige intereses: </label>
-					{
-						tags.map(({id_tags,name}, i) =>
-							<div className="w-full sm:w-auto" key={i}>
-								<label className="inline-flex items-center">
-									<input className="form-radio" type="checkbox" id="tags_act" name="tags_act" value={id_tags}/>
-									<span className="ml-2">{name}</span>
-								</label>
+						<div className="flex flex-col justify-between space-y-4">
+
+							<img className='object-cover w-108 h-60 rounded-lg' src={urlPath}/>
+							<div className="flex flex-col space-y-1">
+									<div className='flex flex-row justify-start space-x-0.5'>
+										<p className='text-sm text-red-500 font-semibold'>*</p>
+										<p className="text-sm font-medium">URL de imagen</p>
+									</div>
+									<div className='flex flex-row justify-start space-x-2'>
+									<input
+										className="input w-full"
+										value = {urlPath}
+										onChange = { (e) => setUrlPath(e.target.value)}
+										required
+									/>
+									<button 
+									type='button'
+										className='btn-terciary w-24'
+										onClick={() => setUrlPath('')}
+									>
+										Borrar
+									</button>
+									</div>
+								</div>
+
+							<div className="flex flex-col space-y-1">
+								<div className='flex flex-row justify-start space-x-0.5'>
+									<p className='text-sm text-red-500 font-semibold'>*</p>
+									<p className="text-sm font-medium">Título</p>
+								</div>
+								<input
+									className="input w-full"
+									value = {titleValue}
+									onChange = { (e) => setTitle(e.target.value)}
+									required
+								/>
 							</div>
-						)
-					}
-				</div>
-				<div>
-					<label htmlFor="postalcode">Codigo Postal </label>
-					<input className="rounded-lg border border-gray-600 focus:border-gray-600" type="text" id="postalcode" name="postalcode" placeholder="Codigo Postal" ref={postalcode}/>
-				</div>
-				<div>
-					<label htmlFor="location">Localidad </label>
-					<input className="rounded-lg border border-gray-600 focus:border-gray-600" type="text" id="location" name="location" placeholder="Localidad" ref={actlocation}/>
-				</div>
-				<div>
-					<label htmlFor="direction">Direccion </label>
-					<input className="rounded-lg border border-gray-600 focus:border-gray-600" type="text" id="direction" name="direction" placeholder="Direccion" ref={direction}/>
-				</div>
-				<div onClick={() => handleSubmit()} className="rounded-full border-2 flex flex-row justify-center cursor-pointer border-orange-500 hover:border-orange-500">Crear</div>		
-			</form>
 
-		</div>
+							{/*Descripción */}
+							<div className="flex flex-col space-y-1">
+								<div className='flex flex-row justify-start space-x-0.5'>
+									<p className='text-sm text-red-500 font-semibold'>*</p>
+									<p className="text-sm font-medium">Descripción</p>
+								</div>
+								<textarea className="w-full h-24 resize-none p-3 bg-gray-50 border border-gray-200 focus:border-orange-500 focus:ring-4 focus:ring-orange-100 rounded-lg appearance-none outline-none"
+									value = {descriptionValue}
+									onChange = { (e) => setDescription(e.target.value)} 
+									required
+									autoFocus
+								/>
+							</div>
+
+							{/**Seats */}
+							<div className='flex flex-row space-x-1'>
+								<div className="flex flex-col space-y-1">
+									<div className='flex flex-row justify-start space-x-0.5'>
+										<p className='text-sm text-red-500 font-semibold'>*</p>
+										<p className="text-sm font-medium">Plazas</p>
+									</div>
+									<input
+										className="input w-full"
+										value = {seatsValue}
+										onChange = { (e) => setSeats(e.target.value)}
+										required
+									/>
+								</div>
+								<div className="flex flex-col space-y-1">
+									<div className='flex flex-row justify-start space-x-0.5'>
+										<p className='text-sm text-red-500 font-semibold'>*</p>
+										<p className="text-sm font-medium">Precio</p>
+									</div>
+									<input
+										className="input w-full"
+										value = {priceValue}
+										onChange = { (e) => setPrice(e.target.value)}
+										required
+									/>
+								</div>
+								<div className="flex flex-col space-y-1">
+									<div className='flex flex-row justify-start space-x-0.5'>
+										<p className='text-sm text-red-500 font-semibold'>*</p>
+										<p className="text-sm font-medium">Duración (min)</p>
+									</div>
+									<input
+										className="input w-full"
+										value = {durationValue}
+										onChange = { (e) => setDuration(e.target.value)}
+										required
+									/>
+								</div>
+							</div>
+
+							{/**Fecha */}
+							<div className="flex flex-col space-y-1">
+								<div className="flex flex-col">
+									<label className="text-sm font-medium text-gray-700">Fecha </label>
+									<input type="date" id="dateMax" name="dateMax" className="input w-full"
+										value={formatDate(dateValue)}
+										onChange = { (e) => setDate(e.target.value)}
+									></input>
+								</div>
+							</div>
+
+							{/*Dirección */}
+							<div className="flex flex-col space-y-1">
+								<div className='flex flex-row justify-start space-x-0.5'>
+									<p className='text-sm text-red-500 font-semibold'>*</p>
+									<p className="text-sm font-medium">Dirección</p>
+								</div>
+								<input
+									className="input w-full"
+									value = {directionValue}
+									onChange = { (e) => setDirection(e.target.value)}
+									required
+								/>
+							</div>
+
+							<div className='flex flex-row space-x-2'>
+								<div className="flex flex-col space-y-1">
+									<div className='flex flex-row justify-start space-x-0.5'>
+										<p className='text-sm text-red-500 font-semibold'>*</p>
+										<p className="text-sm font-medium">Código Postal</p>
+									</div>
+									<input
+										className="input w-full"
+										value = {codPosValue}
+										onChange = { (e) => setCodPos(e.target.value)}
+										required
+									/>
+								</div>
+
+								<div className="flex flex-col space-y-1">
+									<div className='flex flex-row justify-start space-x-0.5'>
+										<p className='text-sm text-red-500 font-semibold'>*</p>
+										<p className="text-sm font-medium">Localidad</p>
+									</div>
+									<input
+										className="input w-full"
+										value = {locationValue}
+										onChange = { (e) => setLocation(e.target.value)}
+										required
+									/>
+								</div>
+							</div>
+
+							<div className="flex flex-col pl-2">
+
+								<div className="flex flex-row">
+									<div className="flex flex-col">
+										<label className="text-sm font-medium">Categorías</label>
+									</div>
+								</div>
+								
+								<div className="flex flex-col justify-center pb-1">	
+									{
+										tags.map(({id_tags,name}, i) =>		
+											<div className="flex flex-row items-center w-60" key={i}>
+												<input className="items-center justify-center mr-2" type="checkbox" id="tags_act" name="tags_act" value={id_tags}/>
+												<label className="text-base font-regular text-gray-700">{name}</label>
+											</div>
+											
+										)
+									}
+								</div>
+
+							</div> 
+
+							<div className="flex flex-row justify-between space-x-4 items-center">
+									<button
+										type="button"
+										className="btn-terciary w-full"
+										onClick={()=> handleCancel()}
+									>
+										Cancelar
+									</button>
+									<button 
+										type="submit" 
+										className="btn-primary w-full"
+									>
+										Crear
+									</button>
+							</div>
+
+						</div>
+					</div>
+				</form>
+			</div>
+		</>
 	)
 }
-
 export async function getServerSideProps(){
 
 	const tags = await fetch(`${url}/api/getAllTags`)
